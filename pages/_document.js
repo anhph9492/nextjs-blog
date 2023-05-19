61c618e4767d2a8740e871fd4fe27a4dd77c9db2
anhph9492 <phamhoanganh9492@gmail.com>
2023-05-19 16:16:21 +0700

    learning nextjs

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        {/* <h1>ahihihihihih</h1> */}
+      </body>
+    </Html>
+  );
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+import FirstPostComponent from "../../src/modules/posts";
+
+export default function FirstPost() {
+  return <FirstPostComponent />;
+}
